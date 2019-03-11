92e45f83a109101e9161949b1fdda01800c44c8f
criedl1 <christoph.riedl1@gmail.com>
2019-03-11 12:53:16 +0100

    Initialized in MainActivity       textview txtText, txtServerText, txtServerAnswer;       edittext edtxtMatNum;       button btnSend;

--- app/src/main/java/michaelknight/myapplication/MainActivity.java
+++ app/src/main/java/michaelknight/myapplication/MainActivity.java
@@ -2,8 +2,19 @@ package michaelknight.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    TextView txtText = findViewById(R.id.txtText);
+    TextView txtServerText = findViewById(R.id.txtServerText);
+    TextView txtServerAnswer = findViewById(R.id.txtServerAnswer);
+    EditText edtxtMatNum = findViewById(R.id.edtxtMatNum);
+    Button btnSend = findViewById(R.id.btnSend);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
